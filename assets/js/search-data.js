b2720cedfdef46f9f4a62398444c96639b97c742
hwiyoungstat <42119627+hwiyoungstat@users.noreply.github.com>
2025-08-14 06:58:37 +0000

    Deploying to gh-pages from @ hwiyoungstat/hwiyoungstat.github.io@b92cd92d35b96c33dc7322bed5ec737b9da43c1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,11 +44,6 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
