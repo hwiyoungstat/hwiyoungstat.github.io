3ec5ca1d847c8b8fd53bde635ca6bb47824442a9
hwiyoungstat <42119627+hwiyoungstat@users.noreply.github.com>
2025-08-14 05:56:46 +0000

    Deploying to gh-pages from @ hwiyoungstat/hwiyoungstat.github.io@913737fadb1736d888b402036710db0a6f071b56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,9 +39,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+          section: "News",},{id: "projects-dcca",
+          title: 'dCCA',
+          description: "differential Canonical Correlation Analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
